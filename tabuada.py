dc54dab00643cb7616cdd667d1140155ce17f420
AyslanBatista <ayslan.batista@hotmail.com>
2022-08-08 14:31:55 -0300

    Correção de Formatação

--- tabuada.py
+++ tabuada.py
@@ -34,7 +34,8 @@ for n1 in numeros :
     for n2 in numeros:
         resultado = n1 * n2
         print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
-    print("\n", "#" * 18,"\n")
+    
+    print("#" * 18,"\n")
     
 
 
