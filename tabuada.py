b1106d7729aa7bc1a87ee9627d48b0624be2fb5a
AyslanBatista <ayslan.batista@hotmail.com>
2022-08-08 13:57:03 -0300

    Melhorando a Formtação do programa

--- tabuada.py
+++ tabuada.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10
 
-Tabuada do: 1
-1 x 1 = 1
-1 x 2 = 2
-1 x 3 = 3
+---Tabuada do: 1---
+
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
 ...
-----------------
-Tabuada do: 2
-2 x 1 = 2
-2 x 2 = 4
-2 x 3 = 6
+###################
+---Tabuada do: 2---
+
+    2 x 1 = 2
+    2 x 2 = 4
+    2 x 3 = 6
 ...
-----------------
+###################
 """
 __version__ = "0.1.0"
 __author__ = "Ayslan"
 __license__ = "Unlicense"
+
 
 # numeros = [1,2,3,4,5,6,7,8,9,10]
 # Transformando uma range em uma lista, range("inicio", "termino")
@@ -25,8 +28,28 @@ numeros = list(range(1,11))
 # Iterable (percorriveis)
 
 # para cada numero em numeros:
-for numero in numeros:
-    print(f"Tabuada do: {numero}")
-    for outro_numero in numeros:
-        print(f"{numero} x {outro_numero} = {numero * outro_numero}")
-    print("-" * 25)
+'''Sem template'''
+for n1 in numeros :
+    print("{:-^18}".format(f"Tabuada do {n1}"), "\n")
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("\n", "#" * 18,"\n")
+    
+
+
+'''Utilizando template'''
+# template = """
+# ---Tabuada do: {numero}---
+
+# {bloco:^18}
+
+# ###################
+# """
+
+# for n1 in numeros:
+#     bloco = ""
+#     for n2 in numeros:
+#         resultado = n1 * n2
+#         bloco += f"    {n1} x {n2} = {resultado}\n" 
+#     print(template.format(numero=n1, bloco=bloco))
