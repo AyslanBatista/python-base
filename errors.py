0c59ecd5c79dcc53347855e76ecb94d870223859
ayslan batista <ayslan.batista@hotmail.com>
2022-08-13 09:47:47 -0300

    Erro: LBYL + Race Condition

--- errors.py
+++ errors.py
@@ -4,7 +4,20 @@ __version__ = "0.1.0"
 __author__ = "Ayslan"
 __license__ = "Unlicense"
 
-names = open("names.txt").readlines()
+import os
+import sys
 
-print(names[2])
+# LBYL - Look Before You Leap >> Olhe antes de atravessar a rua | Antes de fazer uma operação verifique se é possível fazer essa operação
+if os.path.exists("names.txt"):
+    print("O arquivo existe")
+    input("...")  # Race Condition
+    names = open("names.txt").readlines()
+else:
+    print("[Error]: File names.txt not found.")
+    sys.exit(1)
 
+if len(names) >= 3:
+    print(names[2])
+else:
+    print("[Error]: Missing name in the list")
+    sys.exit(1)
